bc9346488866b5e8037932c7231c3043cd6e6bf0
Luke Vachtl <129832830+SpaceSpaguette@users.noreply.github.com>
2024-10-15 07:31:05 +0200

    Added new things

---
+++ Langs/Python/OOP/Me/Test.py
@@ -0,0 +1,12 @@
+import pygame
+import sys
+
+pygame.init()
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+  
+    
---
+++ Langs/Python/PasswordCrack.py
@@ -0,0 +1,17 @@
+'''what we know. is that last number of any 
+
+'''
+#Simple values
+inter = 0
+
+#The bruteforce attack.
+for x in range(2):
+  for y in range(6):
+    for z in range(7):
+      for a in range(8):
+        inter += 1
+        if x < y < z < a:
+          if x+y+z+a == 13:
+            print(f"{x}{y}{z}{a}", end=" ")
+print(f"total iteracions:{inter}")
+
